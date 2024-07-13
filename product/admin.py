e789c6b094823e1764907063059f4fddb00fac91
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2024-07-13 12:46:47 +0600

    Create product, category and slider model, insert product to database

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import (Category, Product, Slider)
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('title',)}
+    list_display = ['id','category','title','price','description','thumbnail']
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('title',)}
+    list_display = ['id','title','slug']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Slider)
---
+++ product/management/commands/create_data.py
@@ -0,0 +1,25 @@
+import requests
+from django.core.management import BaseCommand
+from django.utils.text import slugify
+from product.models import Product, Category
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        response = requests.get('https://fakestoreapi.com/products').json()
+
+        for product in response:
+            category, _ = Category.objects.get_or_create(
+                title = product['category'],
+                slug = slugify(product['category']),
+                featured = True
+            )
+            Product.objects.create(
+                category = category,
+                title = product['title'],
+                slug = slugify(product['title']),
+                price = product['price'],
+                thumbnail = product['image'],
+                description = product['description'],
+
+            )
+        print('Product created')
